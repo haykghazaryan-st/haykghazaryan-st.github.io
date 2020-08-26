033c6b85723b1831c6fe7791e5403aaf2435558e
sasuntitan <sstepanyan@servicetitan.com>
2020-08-26 18:57:47 +0400

    sticky header incomplete

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
+  // var header = document.getElementById("header");
+  // var main = document.getElementsByTagName("main")[0];
+  // var sticky = main.offsetTop;
+
+  // window.onscroll = function () {
+  //   myFunction();
+  // };
+
+  // function myFunction() {
+  //   if (window.pageYOffset > sticky) {
+  //     header.classList.add("sticky");
+  //   } else {
+  //     header.classList.remove("sticky");
+  //   }
+  // }
   $(".slider").slick({
     slidesToShow: 2,
-    appendArrows: $('.slider-arrows'),
-    prevArrow: $('.slie-prev'),
-    nextArrow: $('.slie-next')
+    appendArrows: $(".slider-arrows"),
+    prevArrow: $(".slie-prev"),
+    nextArrow: $(".slie-next"),
   });
 });
